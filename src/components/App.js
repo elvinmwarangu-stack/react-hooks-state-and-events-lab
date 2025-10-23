915cb220f3fcf3cea2193edac3131ad6bf007834
Elvin <elvin.mwarangu@student.moringaschool.com>
2025-10-23 20:36:37 +0300

    gave solutions

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,11 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleClick() {
+    setIsDarkMode(!isDarkMode);
+  }
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+  const itemToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+    return item.category === selectedCategory;
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+
+        <select name="filter" 
+        value={selectedCategory}
+        onChange={handleCategoryChange}>
+
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,8 +27,11 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {itemToDisplay.map((item) => (
+          <Item 
+          key={item.id} 
+          name={item.name} 
+          category={item.category} />
         ))}
       </ul>
     </div>
